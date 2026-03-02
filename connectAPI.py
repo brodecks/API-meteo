e5bc30b7d5a96780e38289a7b81cb298885304cb
brodeck <raphael.donikian@outlook.fr>
2026-03-02 13:38:53 +0100

    2 : Récupération du nom, de la température et de l'état du temps dehors (ex : nuageux, ensoleillé...)

--- connectAPI.py
+++ connectAPI.py
@@ -1,1 +1,32 @@
 import requests
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+if not API_KEY:
+    print("La clé n'a pas pu être récupérée dans le .env.")
+    exit(1)
+
+def getTemperature(ville):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={ville}&appid={API_KEY}&units=metric&lang=fr"
+
+    reponse = requests.get(url)
+    donnees = reponse.json()
+    heure_mesure = datetime.fromtimestamp(donnees['dt']).strftime('%H:%M:%S')
+    print(f"Données relevées à : {heure_mesure}")
+
+    villeInput = donnees['name']
+    tempVille = donnees['main']['temp']
+    tempsCiel = donnees['weather'][0]['description']
+
+    tabVille = {
+        'ville': villeInput,
+        'temperature': tempVille,
+        'temps_dehors': tempsCiel
+    }
+
+    return tabVille
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello world')
+import tkinter as tk
+from connectAPI import getTemperature
+
+
+ville = input("Entrez le nom de la ville : ")
+
+data = getTemperature(ville)
+
+print(f"A {data['ville']}, il fait {data['temperature']} degrès, et voici le temps de dehors : {data['temps_dehors']}.")
