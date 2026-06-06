404564161b51b3d03727e354020fbb9ec76bdcfc
brodeck <raphael.donikian@outlook.fr>
2026-06-06 23:56:26 +0200

    4 : update temps dehors

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import tkinter as tk
 from connectAPI import getTemperature
+import tkinter as tk
 
 
 ville = input("Entrez le nom de la ville : ")
 
 data = getTemperature(ville)
 
-print(f"A {data['ville']}, il fait {data['temperature']} degrès, et voici le temps de dehors : {data['temps_dehors']}.")
+print(f"A {data['ville']}, il fait {data['temperature']} degrès.")
 
-#creer page d'accueil avec champ de saisi d'une ville
+if 'nuageux' in data['temps_dehors']:
+    print("Le temps de dehors est nuageux")
