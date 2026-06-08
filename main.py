c93a88b77cb5ad40dc90f71d69de8b909cd22872
Brodecks <raphael.donikian@outlook.fr>
2026-06-08 23:52:53 +0200

    5 : next -> interface + archi pro

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ ville = input("Entrez le nom de la ville : ")
 data = getTemperature(ville)
 
 print(f"A {data['ville']}, il fait {data['temperature']} degrès.")
-
 if 'nuageux' in data['temps_dehors']:
-    print("Le temps de dehors est nuageux")
+    print("Le temps de dehors est nuageux.")
+elif 'couvert' in data['temps_dehors']:
+    print("Le temps de dehors est couvert.")
+#coder interface avec tableau de chaque temps
